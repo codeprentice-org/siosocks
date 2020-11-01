98f1c61b5a4d9de6937b3404e258562f74991fdf
Evan Mesterhazy <evan.mesterhazy@gmail.com>
2020-11-01 15:09:27 -0500

    WIP: Twisted unit tests

--- tests/test_twisted.py
+++ tests/test_twisted.py
@@ -1,8 +1,10 @@
 import asyncio
+from twisted.internet import reactor
 from asyncio import StreamReader, StreamWriter
 from typing import AsyncIterable, Awaitable, Callable, Optional, Tuple
 
 import pytest
+from twisted.internet.endpoints import TCP4ClientEndpoint
 
 from siosocks.exceptions import SocksException
 from siosocks.io import twisted
@@ -34,7 +36,7 @@ async def endpoint_port(unused_tcp_port_factory: Callable[[], int]) -> AsyncIter
         writer.write(data=data)
         await writer.drain()
         writer.close()
-    
+
     async for port in port_using(
             unused_tcp_port_factory=unused_tcp_port_factory,
             handler=handler,
@@ -52,50 +54,43 @@ async def socks_server_port(unused_tcp_port_factory: Callable[[], int]) -> Async
 
 
 async def open_connection(
+        endpoint,
         port: int,
-        socks_host: Optional[str] = None,
-        socks_port: Optional[int] = None,
         socks_version: Optional[int] = None,
 ) -> Tuple[StreamReader, StreamWriter]:
-    return await twisted.open_connection(
+    return twisted.open_connection(
+            endpoint=endpoint,
             host=HOST,
             port=port,
-            socks_host=socks_host,
-            socks_port=socks_port,
             socks_version=socks_version,
     )
 
 
-@pytest.mark.asyncio
-async def test_connection_direct_success(endpoint_port):
-    reader, writer = await open_connection(
-            port=endpoint_port,
-    )
-    writer.write(MESSAGE)
-    m = await reader.read(8192)
-    assert m == MESSAGE
-
-
-@pytest.mark.asyncio
+# @pytest.mark.asyncio
 async def test_connection_socks_success(endpoint_port, socks_server_port):
-    reader, writer = await open_connection(
-            port=endpoint_port,
-            socks_host=HOST,
-            socks_port=socks_server_port,
-            socks_version=4,
+    ep = TCP4ClientEndpoint(reactor, HOST, socks_server_port)
+    io_deferred = twisted.open_connection(
+        endpoint=ep,
+        host=HOST,
+        port=endpoint_port,
+        socks_version=5
     )
-    writer.write(MESSAGE)
+    io_deferred.addCallback(lambda p: p.transport.write(MESSAGE))
+    m = None
+    def temp(p):
+        m = p.read()
+    io_deferred.addCallback(temp)
     m = await reader.read(8192)
     assert m == MESSAGE
-
-
-@pytest.mark.asyncio
-async def test_connection_socks_failed(unused_tcp_port: int, socks_server_port: int):
-    with pytest.raises(SocksException):
-        await open_connection(port=unused_tcp_port, socks_port=socks_server_port)
-
-
-@pytest.mark.asyncio
-async def test_connection_partly_passed_error(endpoint_port: int, socks_server_port: int):
-    with pytest.raises(SocksException):
-        await open_connection(port=endpoint_port, socks_port=socks_server_port)
+#
+#
+# @pytest.mark.asyncio
+# async def test_connection_socks_failed(unused_tcp_port: int, socks_server_port: int):
+#     with pytest.raises(SocksException):
+#         await open_connection(port=unused_tcp_port, socks_port=socks_server_port)
+#
+#
+# @pytest.mark.asyncio
+# async def test_connection_partly_passed_error(endpoint_port: int, socks_server_port: int):
+#     with pytest.raises(SocksException):
+#         await open_connection(port=endpoint_port, socks_port=socks_server_port)
